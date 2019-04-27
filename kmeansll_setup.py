899e837eda86b2b4a45530ac4d72f5662544b515
Bin Han <39442036+BeanHam@users.noreply.github.com>
2019-04-27 13:56:46 -0400

    Add files via upload

---
+++ kmeansll_setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(name='kmeansll',
+      version='0.1',
+      description='Kmeansll Clustering Algorithm',
+      url='https://github.com/BeanHam/kmeansll',
+      author='Bin Han, Lingxi Song',
+      author_email='binhan96816@gmail.com',
+      license='MIT',
+      packages=['kmeansll'],
+      zip_safe=False)
+
